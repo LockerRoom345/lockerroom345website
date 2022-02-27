cf34507c2bb66119d13dc138e0e6bbfa5833f49b
VigneshSwamy <vigneshofficialonly@gmail.com>
2022-02-27 09:53:47 -0500

    updating the interlinking and backend functinalities to maintain inventory

--- frontend/src/component/Admin/ProductList.js
+++ frontend/src/component/Admin/ProductList.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from "react";
 import { DataGrid } from "@material-ui/data-grid";
 import "./productList.css";
+import ReactTooltip from "react-tooltip";
+import InfoIcon from "@mui/icons-material/Info";
 import { useSelector, useDispatch } from "react-redux";
 import {
   clearErrors,
@@ -52,7 +54,7 @@ const ProductList = ({ history }) => {
   }, [dispatch, alert, error, deleteError, history, isDeleted]);
 
   const columns = [
-    { field: "id", headerName: "Product ID", Width: 100, flex: 0.2 },
+    // { field: "id", headerName: "Product ID", Width: 100, flex: 0.2 },
 
     {
       field: "name",
@@ -91,6 +93,28 @@ const ProductList = ({ history }) => {
       },
     },
     {
+      field: "stockinfo",
+      headerName: "Stock Info",
+      minWidth: 50,
+      flex: 0.1,
+      renderCell: (params) => {
+        return (
+          <Fragment>
+            <div
+              data-tip={params.getValue(params.id, "stockinfo")}
+              data-for="toolTip1"
+              data-place="right"
+              data-type="success"
+              data-multiline="true"
+            >
+              <InfoIcon />
+            </div>
+            <ReactTooltip id="toolTip1" />
+          </Fragment>
+        );
+      },
+    },
+    {
       field: "actions",
       headerName: "Actions",
       minWidth: 50,
@@ -101,7 +125,7 @@ const ProductList = ({ history }) => {
           <Fragment>
             <Link to={`/admin/product/${params.getValue(params.id, "id")}`}>
               {/* <EditIcon /> */}
-               <span>Edit</span>
+              <span>Edit</span>
             </Link>
 
             <Button
@@ -135,28 +159,112 @@ const ProductList = ({ history }) => {
   //       )
   //   );
   // }
+  let hashmap = new Map();
+  let mapping = new Map();
+  // let obj = {};
 
-  let stockarray = products.map((x) =>x.ProductSize.map((y) => y.stock).sort((a, b) => a - b)[0]);
-  // stockarray.map((x) => {
-  //   if (x.includes(0)) {
-  //     outstockincr += 1;
+  // let stockarray = products.map((x) => {
+  //   let arr = [];
+  //   x.ProductSize.map((y) => {
+  //     obj.size = y.size;
+  //     obj.stock = y.stock;
+  //     arr.push(obj);
+  //   });
+  //   hashmap.set(x.SubCategory, arr);
+  //   mapping.set(x.name,   hashmap);
+  // });
+
+  let stockarray = products.map((x) => {
+    let arr = [];
+    x.ProductSize.map((y) => {
+      let obj = {};
+      obj.size = y.size;
+      obj.stock = y.stock;
+      arr.push(obj);
+    });
+    hashmap.set(x.SubCategory, arr);
+    mapping.set(x.name, hashmap);
+  });
+
+  //let hash1 = mapping.get("Sweat Shirts");
+  console.log(mapping);
+  //  for(let i=0; i<mapping.size; i++){
+  //   console.log(mapping.keys);
+  //  }
+
+  // console.log([...mapping].filter((values, keys) => {
+  //   values.forEach((k,v)=>k);
+  //   // if(keys === "Sweat Shirts"){
+  //   //   console.log(keys);
+  //   // }
+  // }));
+  // .map(([k]) => k)
+
+  // mapping.forEach((k) => {
+  //   if (k.get("Mens sizing")) {
+  //     k.get("Mens sizing").map((y) => console.log(y.size + ":" + y.stock));
+  //   } else if (k.get("Womens sizing")) {
+  //     k.get("Womens sizing").map((y) => console.log(y.size + ":" + y.stock));
+  //   } else if (k.get("Boys' sizing")) {
+  //     k.get("Boys' sizing").map((y) => console.log(y.size + ":" + y.stock));
+  //   } else if (k.get("Girls' sizing")) {
+  //     k.get("Girls' sizing").map((y) => console.log(y.size + ":" + y.stock));
+  //   } else {
+  //     k.get("Toddler' sizing").map((y) => console.log(y.size + ":" + y.stock));
   //   }
- // }
- // );
+  // });
 
-   console.log("out of stock list", stockarray);
+  //Object.entries(hash1).forEach((k,v) => console.log(v));
+  // hash1.((x) => {
+  //   console.log(x);
+  //   if (k === "Mens sizing") {
+  //     console.log(v);
+  //   } else if (k === "Womens sizing") {
+  //     console.log(v);
+  //   } else if (k === "Boys' sizing") {
+  //     console.log(v);
+  //   } else if (k === "Girls' sizing") {
+  //     console.log(v);
+  //   } else {
+  //     console.log(v);
+  //   }
+  // });
+  // mapping.forEach((x) => {
+  //   console.log(x);
+  //   // if (x.name === "sweat shirts") {
+  //   //   console.log( x.get("Mens sizing"));
+  //   // }
+  // });
+
+  // console.log("out of stock list", map.get("Sweat Shirts").map((x) => {
+  //   return x
+  // }));
 
   //   });
   //  dummy[0].ProductSize.reduce(function(prev, curr) {
   //    console.log(prev.stock < curr.stock ? prev : curr);
   //  });
   //console.log(Object.keys(products.ProductSize[0]).sort((a,b) => (products.ProductSize[a] - products.ProductSize[b])));
+
+  // let array1 = [];
+  // products &&
+  // products.forEach((item) => {
+  //   console.log(item.name)
+  //   item.ProductSize.map((y) => {  (y.size +":" + y.stock)  })
+  // });
+
   products &&
     products.forEach((item) => {
+      console.log(item);
       rows.push({
         id: item._id,
         stock: item.ProductSize.map((y) => y.stock).sort((a, b) => a - b)[0],
         // price: item.price,
+        stockinfo: (item.ProductSize.map((y) => {
+          let str ="";
+          str+=( "{"+ y.size + " : " + y.stock + "} <br />" );
+          return  str;
+        })),
         name: item.name,
         size: item.Size,
         category: item.category,
--- frontend/src/component/Home/ProductCard.js
+++ frontend/src/component/Home/ProductCard.js
@@ -30,45 +30,50 @@ const ProductCard = ({ product, history }) => {
     window.location.href = "http://localhost:3000/requestform";
   };
 
-  const [SubCategory, setSubCategory] = useState("");
-  const [ProductSize, setProductSize] = useState("");
+  const [SubCategory, setSubCategory] = useState(
+    Object.keys(product.hashmap)[0] || ""
+  );
+  const [ProductSize, setProductSize] = useState(0);
 
-  const increaseQuantity = () => {    
-    //let stockarray = products.map((x) => x.ProductSize.map((y) => y.stock).sort((a,b)=>a-b));
-    let maxstock = (product.ProductSize.map((y)=> y.stock).sort((a,b)=>b-a))[0];    
+  const increaseQuantity = () => {       
+    let maxstock = product.hashmap[SubCategory][ProductSize].stock;
     // console.log("product Stock currently",maxstock);
     if (maxstock <= quantity) return;
-    const qty = quantity + 1;
-    console.log("increase quantity to",qty);
+    const qty = quantity + 1;    
     setQuantity(qty);
   };
 
-  const decreaseQuantity = () => {
-    console.log("product Stock currently",product.Stock);
+  const decreaseQuantity = () => {    
     if (1 >= quantity) return;
     const qty = quantity - 1;
-    console.log("decrease quantity to",qty);
+    console.log("decrease quantity to", qty);
     setQuantity(qty);
   };
 
   const handlesubcatChange = (e) => {
     setSubCategory(e.target.value);
-    //console.log((SubCategory[e.target.value]))
   };
 
   const handlesizeChange = (e) => {
     //console.log(product);
+    let k = product.hashmap[SubCategory][e.target.value];
+    // debugger
     setProductSize(e.target.value);
   };
   const addToCartHandler = (e) => {
     e.preventDefault();
-    if (SubCategory.trim() === "" || ProductSize.trim() == "" || SubCategory.trim() === "SelectCategory" || ProductSize.trim() == "selectsize") {
-      alert.show("required Subcategory and Size field");
-    } else {
+    // if (
+    //   // SubCategory.trim() === "" ||
+    //   ProductSize.size.trim() == ""
+    //   // SubCategory.trim() === "SelectCategory" ||
+    //   // ProductSize.trim() == "selectsize"
+    // ) {
+    //   alert.show("required Subcategory and Size field");
+    // } else {
       //console.log("clicked");
-      dispatch(addItemsToCart(product._id, quantity, SubCategory, ProductSize));
+      dispatch(addItemsToCart(product.id, quantity, SubCategory, product.hashmap[SubCategory][ProductSize].size));
       alert.success("Item Added To Cart");
-    }
+    //}
   };
 
   return (
@@ -78,120 +83,101 @@ const ProductCard = ({ product, history }) => {
     //     addToCartHandler(e);
     //   }}
     // >
-      <div className="productCard">
-        <img src={product.images[0].url} alt={product.name} />
+    <div className="productCard">
+      <img src={product.url} alt={product.name} />
 
-        <div className="productCardDesc">
-          {/* {"Product Name :"} */}
-          <div className="productname">
-            <p>{product.name}</p>
+      <div className="productCardDesc">
+        <div className="productname">
+          <p>{product.name}</p>
+        </div>
+        <div className="productCardSpan">
+          <div className="detailsBlock-3-1-1">
+            <button onClick={decreaseQuantity}>-</button>
+            <input readOnly type="number" value={quantity} />
+            <button onClick={increaseQuantity}>+</button>
           </div>
-          {/* <Rating {...options} />{" "} */}
-          <div className="productCardSpan">
-            <div className="detailsBlock-3-1-1">
-              <button onClick={decreaseQuantity}>-</button>
-              <input readOnly type="number" value={quantity} />
-              <button onClick={increaseQuantity}>+</button>
-            </div>
-            {/* ({product.numOfReviews} Reviews) */}
-          </div>
-          <div className="category">
-            <select
-              name="category"
-              id="category"
-              onChange={(e) => {
-                handlesubcatChange(e);
-              }}
-              required
-            >
-              <option value="SelectCategory">Select SubCategory</option>
-              <option value="Mens sizing">Mens sizing</option>
-              <option value="Womens sizing">Womens sizing</option>
-              <option value="Boys' sizing">Boys' sizing</option>
-              <option value="Girls' sizin">Girls' sizing</option>
-              <option value="Toddlers sizing">Toddlers sizing</option>
-            </select>
-          </div>
-          <div className="size">
-            {product && product.ProductSize && (
-              <select
-                name="size"
-                id="size"
-                onChange={(e) => {
-                  handlesizeChange(e);
-                }}
-                required
-              >
-                <option value="selectsize">Select Size</option>
-                {/* {Object.entries(product.ProductSize[0]).map((k,v) =>              
-                <option value={k}>
-                  {k[0]}
-                </option>              
-            )}           */}
-                {product.ProductSize.map((item, index) => (
-                  <option key={item.size} value={item.size}>
-                    {item.size}
-                  </option>
-                ))}
-              </select>
-            )}
-          </div>
+        </div>
+        <div className="category">
+          <select
+            name="category"
+            id="category"
+            value={SubCategory}
+            onChange={(e) => {
+              handlesubcatChange(e);
+            }}
+            required
+          >
+            {Object.keys(product.hashmap).map((size) => (
+              <option value={size}>{size}</option>
+            ))}
+            {/* <option value="SelectCategory">Select SubCategory</option>
+            <option value="Mens sizing">Mens sizing</option>
+            <option value="Womens sizing">Womens sizing</option>
+            <option value="Boys' sizing">Boys' sizing</option>
+            <option value="Girls' sizin">Girls' sizing</option>
+            <option value="Toddlers sizing">Toddlers sizing</option> */}
+          </select>
+        </div>
+        <div className="size">
+          <select
+            name="size"
+            id="size"
+            value={ProductSize}
+            onChange={(e) => {
+              handlesizeChange(e);
+            }}
+            required
+          >
+            {/* <option value="selectsize">Select Size</option> */}
+
+            {product.hashmap[SubCategory].map((item, i) => (
+              <option key={item.size} value={i}>
+                {item.size}
+              </option>
+            ))}
+          </select>
+        </div>
+        {
           <div className="stock">
             <p>
               Status:
               <b
                 className={
-                  product &&
-                  product.ProductSize &&
-                  product.ProductSize.find(
-                    (item) => item.size == ProductSize
-                  ) &&
-                  product.ProductSize.find((item) => item.size == ProductSize)
-                    .stock <= 0
+                  product.hashmap[SubCategory][ProductSize].stock <= 0
                     ? "redColor"
                     : "greenColor"
                 }
               >
-                {product.ProductSize.find((item) => item.size == ProductSize) &&
-                product.ProductSize.find((item) => item.size == ProductSize)
-                  .stock < 1
+                {product.hashmap[SubCategory][ProductSize].stock < 1
                   ? "OutOfStock"
                   : "InStock"}
               </b>
               <br />
-              {/* <button
-                    disabled={product.Stock < 1 ? false : true}                    
-                    onClick={takeToRequestForm}
-                  >Request</button> */}
             </p>
           </div>
-          <div className="addshopcart">
-            <button
-               onClick={addToCartHandler}
+        }
+
+        <div className="addshopcart">
+          <button
+            onClick={addToCartHandler}
+            disabled={
+              product.hashmap[SubCategory][ProductSize].stock < 1
+                ? true
+                : false
+            }
+          >
+            <AddShoppingCartIcon
               disabled={
-                product.ProductSize.find((item) => item.size == ProductSize) &&
-                product.ProductSize.find((item) => item.size == ProductSize)
-                  .stock < 1
+                product.hashmap[SubCategory][ProductSize].stock< 1
                   ? true
                   : false
               }
-            >
-              <AddShoppingCartIcon
-                disabled={
-                  product.ProductSize.find(
-                    (item) => item.size == ProductSize
-                  ) &&
-                  product.ProductSize.find((item) => item.size == ProductSize)
-                    .stock < 1
-                    ? true
-                    : false
-                }
-              />
-              Add to Cart
-            </button>
-          </div>
+            />
+            Add to Cart
+          </button>
         </div>
       </div>
+    </div>
     // </form>
 
     // </Link>
