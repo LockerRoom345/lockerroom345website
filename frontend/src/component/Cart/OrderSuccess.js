5d9258f47651516e13313bc9a5b58748b38f9111
nkulakr9 <nkulakr9@binghamton.edu>
2022-02-27 20:40:24 -0500

    Final push for today

--- frontend/src/component/Cart/OrderSuccess.js
+++ frontend/src/component/Cart/OrderSuccess.js
@@ -3,10 +3,29 @@ import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import "./orderSuccess.css";
 import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import { useSelector} from "react-redux";
+import { useSelector, useDispatch} from "react-redux";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const OrderSuccess = () => {
+
+  
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  async function displayOrder() {
+    const { data } = await axios.get("/api/v1/orders/me");
+    //console.log(data.orders[data.orders.length-1]._id);
+    var orderId = data.orders[data.orders.length-1]._id
+    history.push(`/order/${orderId}`);       
+  }
   const { cartItems } = useSelector((state) => state.cart);
+
+  const { orders } = useSelector((state) => state.allOrders);
+
+  console.log(orders);
+
 
   console.log("change done");
   cartItems.length =0;
@@ -16,8 +35,11 @@ const OrderSuccess = () => {
     <div className="orderSuccess">
       <CheckCircleIcon />
 
+     
+
       <Typography>Your Order has been Placed successfully </Typography>
-      <Link to="/orders">View Orders</Link>
+      <Button onClick={displayOrder}>Display Order</Button>
+      {/* <Link to=`/order/${productId}``>View Orders</Link> */}
     </div>
   );
 };
